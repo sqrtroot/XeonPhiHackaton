b02049b06f9c2d696e67e6fa238abcf4c8d14fb7
Robert <robert.bezem@gmail.com>
2017-05-20 15:29:16 +0200

    lets debug

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 int main(int argc, char* argv[]) {
 	std::vector<Sphere> objects{
-		Sphere(Coordinate(150, 120, -20), 80, Material(Color(0xFF, 0, 0), 0.2, 0.0, 0.0)),
-		Sphere(Coordinate(420, 120, 0), 100, Material(Color(0, 0, 0xFF), 0.8, 0.0, 0.0)),
-		Sphere(Coordinate(320, 240, -40), 50, Material(Color(0, 0xFF, 0), 0.0, 0.0, 0.0)),
-		Sphere(Coordinate(300, 200, 200), 100, Material(Color(0xFF, 0xFF, 0), 0.8, 0.0, 0.0)),
-		Sphere(Coordinate(300, 130, 100), 40, Material(Color(0xFF, 0, 0xFF), 0.0, 0.0, 0.0)),
-		Sphere(Coordinate(300, 1000, 0), 700, Material(Color(0xFF, 0xFF, 0xFF), 0.0, 0.5, 0.0))
+		Sphere(Coordinate(150, 120, -20), 80, Material(Color(255, 0, 0), 0.2, 0.0, 0.0)),
+		Sphere(Coordinate(420, 120, 0), 100, Material(Color(0, 0, 255), 0.8, 0.0, 0.0)),
+		Sphere(Coordinate(320, 240, -40), 50, Material(Color(0, 255, 0), 0.0, 0.0, 0.0)),
+		Sphere(Coordinate(300, 200, 200), 100, Material(Color(255, 255, 0), 0.8, 0.0, 0.0)),
+		Sphere(Coordinate(300, 130, 100), 40, Material(Color(255, 0, 255), 0.0, 0.0, 0.0)),
+		Sphere(Coordinate(300, 1000, 0), 700, Material(Color(255, 255, 255), 0.0, 0.5, 0.0))
 	};
 	std::vector<Coordinate> lights{
 		Coordinate(200, -100, 0), Coordinate(600, 200, -200)
--- ppm.hpp
+++ ppm.hpp
@@ -8,17 +8,14 @@ void pixels_to_ppm (const std::vector<std::vector<Coordinate> >& pixels)
 {
   int i, j;
   FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
-  (void) fprintf(fp, "P6\n%d %d\n255\n", pixels.size(), pixels[0].size());
+  (void) fprintf(fp, "P3 %d %d 255\n", pixels.size(), pixels[0].size());
   for (j = 0; j < pixels.size(); ++j)
     {
       for (i = 0; i < pixels[j].size(); ++i)
 	{
-	  static unsigned char color[3];
-	  color[0] = (char) pixels[j][i].x;  /* red */
-	  color[1] = (char) pixels[j][i].y;  /* green */
-	  color[2] = (char) pixels[j][i].z;  /* blue */
-	  (void) fwrite(color, 1, 3, fp);
+	  fprintf(fp, "%.0f %.0f %.0f ",pixels[j][i].x,pixels[j][i].y,pixels[j][i].z);
 	}
+  fprintf(fp,"\n");
     }
   (void) fclose(fp);
 }
